37e75dcf32806c51be47c802eabdb20236af3246
Gavyn Caldwell <gavyncaldwell@gmail.com>
2017-08-01 10:39:58 -0600

    pretty much finished

--- duplicate-css.js
+++ duplicate-css.js
@@ -13,65 +13,42 @@ module.exports = function(file) {
   return new Promise((resolve, reject) => {
     writeAllScss(file)
 
-    sass.render({file: __dirname + '/styles.scss'}, (err, res) => {
+    sass.render({ file: __dirname + '/styles.scss' }, (err, res) => {
       fs.writeFileSync(__dirname + '/compiled.css', res.css)
 
-      const cssObj = parseCSS(__dirname + '/compiled.css')
+      const filteredObj = {}
+      let cssObj = parseCSS(__dirname + '/compiled.css')
+      cssObj = _.invertBy(cssObj)
 
-      console.log(cssObj)
+      const objKeys = _.keys(cssObj)
+      objKeys.forEach(k => {
+        if (cssObj[k].length > 1) {
+          filteredObj[k] = cssObj[k]
+        }
+      })
+
+      resolve(filteredObj)
+
+      fs.open('duplicate-class-list.txt', 'w', (err, fd) => {
+        if (err) {
+          reject(err)
+        }
+
+        _.keys(filteredObj).forEach(key => {
+          const classes = JSON.stringify(filteredObj[key])
+            .split(',')
+            .join('\n')
+            .split('"')
+            .join('')
+            .split('[')
+            .join('')
+            .split(']')
+            .join('')
+          const text = `${classes}\n\nall have the same css properties:\n\n${key}\n\n========================================\n\n\n`
+          fs.appendFileSync('duplicate-class-list.txt', text, 'utf8')
+        })
+      })
     })
-    // findInFiles
-    //   .find({'term': regEx, 'flags': 'g'}, file, '.scss$')
-    //   .then((results) => {
-    //     fs.writeFile('duplicate-class-list.txt', '')
-    //     const classObj = {}
-    //     const filteredObj = {}
-    //     const realClasses = {}
-    //     let arrOfValues = _.values(results).map((val) => {
-    //       return val.matches
-    //     })
-    //
-    //     arrOfValues = [].concat.apply([], arrOfValues)
-    //
-    //     arrOfValues.forEach((val) => {
-    //       const polished = val.split(' ').join('').split('\n').join('')
-    //       const objKey = polished.split(classReg)[0]
-    //       const objVal = (polished.match(classReg) || [])[0]
-    //
-    //       if (!objKey || !objVal) return
-    //
-    //       classObj[objKey] = objVal
-    //     })
-    //
-    //     _.keys(classObj).map((key) => {
-    //       if (key.indexOf('@') === -1) {
-    //         realClasses[key] = classObj[key]
-    //       }
-    //     })
-    //
-    //     const inverted = _.invertBy(realClasses)
-    //
-    //     _.keys(inverted).forEach((key) => {
-    //       if (inverted[key].length > 1) {
-    //         filteredObj[key] = inverted[key]
-    //       }
-    //     })
-    //
-    //     fs.open('duplicate-class-list.txt', 'w', (err, fd) => {
-    //       if (err) {
-    //         reject(err)
-    //       }
-    //
-    //       _.keys(filteredObj).forEach((key) => {
-    //         const classes =  JSON.stringify(filteredObj[key]).split(',').join('\n').split('"').join('').split('[').join('').split(']').join('')
-    //         const text = `${classes}\n\nall have the same css properties:\n\n${key}\n\n========================================\n\n\n`
-    //         fs.appendFileSync('duplicate-class-list.txt', text, 'utf8')
-    //       })
-    //     })
-    //
-    //     resolve(filteredObj)
-    //   })
-    //   .catch(reject)
   })
 }
 
@@ -80,21 +57,34 @@ function parseCSS(file, callback) {
   const css = fs.readFileSync(file, 'utf8')
   let matches = css.match(regEx)
 
-  matches.forEach((match) => {
-    const polished =  match.split(' ').join('').split('\n').join('')
-    const objKey = polished.split(classReg)[0].split('{').join('').split('}').join('')
-    const objVal = _.pull((polished.match(classReg) || [])[0].split('{').join('').split('}').join('').split(';'), '')
+  matches.forEach(match => {
+    const polished = match.split(' ').join('').split('\n').join('')
+    const objKey = polished
+      .split(classReg)[0]
+      .split('{')
+      .join('')
+      .split('}')
+      .join('')
+    const objVal = _.pull(
+      (polished.match(classReg) || [])[0]
+        .split('{')
+        .join('')
+        .split('}')
+        .join('')
+        .split(';'),
+      ''
+    )
 
     obj[objKey] = objVal
   })
 
   if (obj === {}) return console.log('Something went wrong')
 
-  return obj;
+  return obj
 }
 
 function writeAllScss(file, callback) {
-  glob('/**/*.scss', {root: file}, (err, files) => {
+  glob('/**/*.scss', { root: file }, (err, files) => {
     if (err) return console.log(err)
 
     fs.writeFileSync(__dirname + '/styles.scss', '')
@@ -104,9 +94,15 @@ function writeAllScss(file, callback) {
     writeSpecificFile(files, 'functions')
     writeSpecificFile(files, 'mixins')
 
-    files.map((file) => {
+    files.map(file => {
       const content = fs.readFileSync(file, 'utf8')
-      if (file.indexOf('variables') === -1 && file.indexOf('animations') === -1 && file.indexOf('functions') === -1 && file.indexOf('mixins') === -1 && content.indexOf('@import') === -1) {
+      if (
+        file.indexOf('variables') === -1 &&
+        file.indexOf('animations') === -1 &&
+        file.indexOf('functions') === -1 &&
+        file.indexOf('mixins') === -1 &&
+        content.indexOf('@import') === -1
+      ) {
         fs.appendFileSync(__dirname + '/styles.scss', content)
       }
     })
@@ -114,8 +110,8 @@ function writeAllScss(file, callback) {
 }
 
 function writeSpecificFile(files, query) {
-  files.map((file) => {
-    if(file.indexOf(query) !== -1) {
+  files.map(file => {
+    if (file.indexOf(query) !== -1) {
       const content = fs.readFileSync(file, 'utf8')
       fs.appendFileSync(__dirname + '/styles.scss', content)
     }
--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const duplicateCSS = require('./duplicate-css')
 
 if (process.argv[2] === '--css') {
   duplicateCSS(process.argv[3])
-    .then((response) => {
+    .then(response => {
       const duplicateClasses = _.values(response)
-      console.log(response)
-      console.log(`There are ${duplicateClasses.length} sets of classes that are doing the same thing`)
-      console.log(`You could delete ${_.flattenDeep(duplicateClasses).length - duplicateClasses.length} classes from your css`)
-      console.log('you can open duplicate-class-list.txt to see a more readable list of duplicate classes')
+      console.log(duplicateClasses)
+      console.log(
+        `There are ${duplicateClasses.length} sets of classes that are doing the same thing`
+      )
+      console.log(
+        'you can open duplicate-class-list.txt to see a more readable list of duplicate classes'
+      )
     })
     .catch(console.error)
 } else {
